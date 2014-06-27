baf15bb310762b4d4e211fcd299c18ae2914d581
Jihoon Lee <jihoonlee.in@gmail.com>
2014-06-27 11:25:14 +0900

    apply install rules for new packages

--- yocs_ar_pair_approach/src/yocs_ar_pair_approach/node.py
+++ yocs_ar_pair_approach/src/yocs_ar_pair_approach/node.py
@@ -70,10 +70,10 @@ class Node(object):
           namespace.
         '''
         publishers = {}
-        publishers['result'] = rospy.Publisher('~result', std_msgs.Bool)
-        publishers['initial_pose_trigger'] = rospy.Publisher('~initialise', std_msgs.Empty)
-        publishers['enable_approach_controller'] = rospy.Publisher('~enable_approach_controller', std_msgs.Empty)
-        publishers['disable_approach_controller'] = rospy.Publisher('~disable_approach_controller', std_msgs.Empty)
+        publishers['result'] = rospy.Publisher('~result', std_msgs.Bool, queue_size=100)
+        publishers['initial_pose_trigger'] = rospy.Publisher('~initialise', std_msgs.Empty, queue_size=100)
+        publishers['enable_approach_controller'] = rospy.Publisher('~enable_approach_controller', std_msgs.Empty, queue_size=100)
+        publishers['disable_approach_controller'] = rospy.Publisher('~disable_approach_controller', std_msgs.Empty, queue_size=100)
         subscribers = {}
         subscribers['enable'] = rospy.Subscriber('~enable', std_msgs.Bool, self._ros_enable_subscriber)
         subscribers['spotted_markers'] = rospy.Subscriber('~spotted_markers', std_msgs.String, self._ros_spotted_subscriber)
