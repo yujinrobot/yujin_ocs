b3041ac822539dfd8cba045a883492d4ca93f7cb
Jihoon Lee <jihoonlee.in@gmail.com>
2014-10-01 16:17:27 +0900

    updates

--- yocs_docking_interactor/src/yocs_docking_interactor/utils.cpp
+++ yocs_docking_interactor/src/yocs_docking_interactor/utils.cpp
@@ -9,12 +9,12 @@ namespace yocs_docking_interactor {
 
 void DockingInteractor::loginfo(const std::string& msg)
 {
-  ROS_INFO_STREAM_NAMED(ros::this_node::getName(), msg);
+  ROS_INFO_STREAM(ros::this_node::getName() << " : " << msg);
 }
 
 void DockingInteractor::logwarn(const std::string& msg)
 {
-  ROS_WARN_STREAM_NAMED(ros::this_node::getName(), msg);
+  ROS_WARN_STREAM(ros::this_node::getName() << " : " << msg);
 }
 
 void DockingInteractor::sendMovebaseGoal(const geometry_msgs::PoseStamped& pose)
