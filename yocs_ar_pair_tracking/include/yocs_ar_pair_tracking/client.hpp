c1daa77a0f45711ba62c31d4994eb1be903628fa
Daniel Stonier <d.stonier@gmail.com>
2016-01-27 17:13:12 +0900

    [yocs_ar_pair_tracking] eclipse files

--- yocs_ar_pair_tracking/include/yocs_ar_pair_tracking/client.hpp
+++ yocs_ar_pair_tracking/include/yocs_ar_pair_tracking/client.hpp
@@ -2,6 +2,7 @@
 
 #ifndef _YOCS_AR_PAIR_TRACKING_CLIENT_HPP_
 #define _YOCS_AR_PAIR_TRACKING_CLIENT_HPP_
+
 #include <ros/ros.h>
 
 #include <tf/tf.h>
@@ -10,7 +11,7 @@
 
 #include <ar_track_alvar_msgs/AlvarMarkers.h>
 #include <yocs_msgs/ARPairList.h>
-#include <yocs_math_toolkit/common.hpp>                                                                        
+#include <yocs_math_toolkit/common.hpp>
 #include <yocs_math_toolkit/geometry.hpp>
 
 
@@ -36,12 +37,12 @@ class ARPairTrackingClient {
     tf::TransformListener tf_listener_;
     tf::TransformBroadcaster tf_brcaster_;
     double tf_broadcast_freq_;
-    
+
     std::string global_frame_;
     std::string global_marker_prefix_;
     std::string target_frame_postfix_;
     double baseline_;
-    double target_offset_; 
+    double target_offset_;
 };
 }
 
