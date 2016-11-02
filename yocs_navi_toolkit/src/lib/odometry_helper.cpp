f1b6a8ef7a9fe84c9279db78d6b5ca04ef6dc32e
Alexander Reimann <A.Reimann@yujinrobot.com>
2016-11-02 17:25:41 +0900

    [navi_toolkit] OdometryHelper checks / info if odom is available

--- yocs_navi_toolkit/src/lib/odometry_helper.cpp
+++ yocs_navi_toolkit/src/lib/odometry_helper.cpp
@@ -29,31 +29,74 @@ OdometryHelper::~OdometryHelper() {}
 void OdometryHelper::odometryCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
   std::lock_guard<std::mutex> lock(data_mutex_);
-  odometry_ = *msg;
+
+  if(!odometry_)
+  {
+    odometry_ = std::unique_ptr<nav_msgs::Odometry>(new nav_msgs::Odometry());
+  }
+
+  *odometry_ = *msg;
 }
 
-nav_msgs::Odometry OdometryHelper::odometry()
+bool OdometryHelper::initialized()
 {
   std::lock_guard<std::mutex> lock(data_mutex_);
-  nav_msgs::Odometry msg = odometry_;
+  if(odometry_)
+  {
+    return true;
+  }
+
+  return false;
+}
+
+std::shared_ptr<nav_msgs::Odometry> OdometryHelper::odometry()
+{
+  if(!initialized())
+  {
+    return std::shared_ptr<nav_msgs::Odometry>();
+  }
+
+  std::lock_guard<std::mutex> lock(data_mutex_);
+  std::shared_ptr<nav_msgs::Odometry> msg = std::make_shared<nav_msgs::Odometry>(*odometry_);
   return msg;
 }
 
-void OdometryHelper::position(Eigen::Vector3f& position) {
+bool OdometryHelper::position(Eigen::Vector3f& position) {
+  if(!initialized())
+  {
+    return false;
+  }
+
   std::lock_guard<std::mutex> lock(data_mutex_);
-  position << odometry_.pose.pose.position.x, odometry_.pose.pose.position.y, odometry_.pose.pose.position.z;
+  position << odometry_->pose.pose.position.x, odometry_->pose.pose.position.y, odometry_->pose.pose.position.z;
+
+  return true;
 }
 
-void OdometryHelper::yaw(float& angle) {
+bool OdometryHelper::yaw(float& angle) {
+  if(!initialized())
+  {
+    return false;
+  }
+
   std::lock_guard<std::mutex> lock(data_mutex_);
-  angle = tf::getYaw(odometry_.pose.pose.orientation);
+  angle = tf::getYaw(odometry_->pose.pose.orientation);
+
+  return true;
 }
 
-void OdometryHelper::velocities(std::pair<float, float>& mobile_twist_velocities)
+bool OdometryHelper::velocities(std::pair<float, float>& mobile_twist_velocities)
 {
+  if(!initialized())
+  {
+    return false;
+  }
+
   std::lock_guard<std::mutex> lock(data_mutex_);
-  mobile_twist_velocities.first = odometry_.twist.twist.linear.x;
-  mobile_twist_velocities.second = odometry_.twist.twist.angular.z;
+  mobile_twist_velocities.first = odometry_->twist.twist.linear.x;
+  mobile_twist_velocities.second = odometry_->twist.twist.angular.z;
+
+  return true;
 }
 
 /*****************************************************************************
